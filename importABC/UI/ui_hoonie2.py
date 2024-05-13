9b77246756f2902eca54c99b8bdc748bcc75cd77
LeeSeungah <seungah_lee@keyringstudio.com>
2024-05-13 16:03:56 +0900

    ui edit

--- importABC/UI/ui_hoonie2.py
+++ importABC/UI/ui_hoonie2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'hoonie2_uiOLvFem.ui'
+## Form generated from reading UI file 'hoonie2_uiufpKKz.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -13,18 +13,18 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
 
-class Ui_HOONIE_ver02(object):
-    def setupUi(self, HOONIE_ver02):
-        if not HOONIE_ver02.objectName():
-            HOONIE_ver02.setObjectName(u"HOONIE_ver02")
-        HOONIE_ver02.resize(970, 820)
-        self.verticalLayout_2 = QVBoxLayout(HOONIE_ver02)
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(970, 820)
+        self.verticalLayout_2 = QVBoxLayout(Form)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.label = QLabel(HOONIE_ver02)
+        self.label = QLabel(Form)
         self.label.setObjectName(u"label")
         font = QFont()
         font.setFamily(u"\ub098\ub214\uace0\ub515")
@@ -35,7 +35,7 @@ class Ui_HOONIE_ver02(object):
 
         self.horizontalLayout_3.addWidget(self.label)
 
-        self.btn_reload = QPushButton(HOONIE_ver02)
+        self.btn_reload = QPushButton(Form)
         self.btn_reload.setObjectName(u"btn_reload")
         self.btn_reload.setMinimumSize(QSize(150, 30))
         self.btn_reload.setMaximumSize(QSize(150, 30))
@@ -48,7 +48,7 @@ class Ui_HOONIE_ver02(object):
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.tabWidget = QTabWidget(HOONIE_ver02)
+        self.tabWidget = QTabWidget(Form)
         self.tabWidget.setObjectName(u"tabWidget")
         font1 = QFont()
         font1.setPointSize(10)
@@ -89,12 +89,12 @@ class Ui_HOONIE_ver02(object):
 
         self.horizontalLayout_4 = QHBoxLayout()
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.label_2 = QLabel(HOONIE_ver02)
+        self.label_2 = QLabel(Form)
         self.label_2.setObjectName(u"label_2")
 
         self.horizontalLayout_4.addWidget(self.label_2)
 
-        self.btn_shotgrid = QPushButton(HOONIE_ver02)
+        self.btn_shotgrid = QPushButton(Form)
         self.btn_shotgrid.setObjectName(u"btn_shotgrid")
         self.btn_shotgrid.setMinimumSize(QSize(150, 30))
         self.btn_shotgrid.setMaximumSize(QSize(150, 30))
@@ -108,21 +108,21 @@ class Ui_HOONIE_ver02(object):
         self.verticalLayout_2.addLayout(self.verticalLayout)
 
 
-        self.retranslateUi(HOONIE_ver02)
+        self.retranslateUi(Form)
 
         self.tabWidget.setCurrentIndex(0)
 
 
-        QMetaObject.connectSlotsByName(HOONIE_ver02)
+        QMetaObject.connectSlotsByName(Form)
     # setupUi
 
-    def retranslateUi(self, HOONIE_ver02):
-        HOONIE_ver02.setWindowTitle(QCoreApplication.translate("HOONIE_ver02", u"HOONIE_ver02", None))
-        self.label.setText(QCoreApplication.translate("HOONIE_ver02", u"Hoonie ver2", None))
-        self.btn_reload.setText(QCoreApplication.translate("HOONIE_ver02", u"Reload", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("HOONIE_ver02", u"Publish", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("HOONIE_ver02", u"Imported", None))
-        self.label_2.setText(QCoreApplication.translate("HOONIE_ver02", u"24.05.13", None))
-        self.btn_shotgrid.setText(QCoreApplication.translate("HOONIE_ver02", u"Open page Shotgrid", None))
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"HOONIE_ver02", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Hoonie ver2", None))
+        self.btn_reload.setText(QCoreApplication.translate("Form", u"Reload", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("Form", u"Publish", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("Form", u"Imported", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"24.05.13", None))
+        self.btn_shotgrid.setText(QCoreApplication.translate("Form", u"Open page Shotgrid", None))
     # retranslateUi
 
